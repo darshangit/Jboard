93a2afaed68ca960b9885a4cc44c11eeb1ab68d8
darshangit <darshans1989@gmail.com>
2018-03-18 08:39:38 +0530

    Scrum Attendance: Functionality Completed. Integrated with the backend.

--- src/app/scrum-attendance/scrum.component.ts
+++ src/app/scrum-attendance/scrum.component.ts
@@ -28,40 +28,62 @@ export class ScrumComponent {
   monthsDropdown = [];
 
   constructor(private scrumService: ScrumAttendanceService) {
-    this.initialLoad(this.getNoOfDays());
+    this.initialLoad(moment(moment()).daysInMonth());
     this.loadMonthsDropdown();
   }
 
   initialLoad(noOfDaysInThisMonth): any {
-    this.ft1Value = null;
-    this.ft2Value = null;
-
-    this.labelArray = [];
-
     const momentDate = moment(new Date());
     const yearAndMonth = momentDate.format('MMM YY');
 
-    for (let index = 0; index < noOfDaysInThisMonth; index++) {
+    this.populateXAxisLabels(noOfDaysInThisMonth);
+    this.loadValuesBasedOnMonthYear(yearAndMonth, false);
+  }
+
+  populateXAxisLabels(noOfDays) {
+    this.labelArray = [];
+
+    for (let index = 0; index < noOfDays; index++) {
       this.labelArray.push(index);
     }
     this.labelArray.push(this.labelArray[this.labelArray.length - 1] + 1);
+  }
 
-    this.scrumService.getScrumAttendance(yearAndMonth.replace(/\s/g, '')).subscribe(response => {
+  loadValuesBasedOnMonthYear(yearAndMonth: string, isPastMonths: boolean) {
+    this.ft1Value = null;
+    this.ft2Value = null;
+    this.ft1Data = [0];
+    this.ft2Data = [0];
+
+    this.scrumService
+      .getScrumAttendance(yearAndMonth.replace(/\s/g, ''))
+      .subscribe(response => {
         response.forEach(scEnt => {
-            this.ft1Data[scEnt.day] = scEnt.ft1Attendance;
-            this.ft2Data[scEnt.day] = scEnt.ft2Attendance;
+          this.ft1Data[scEnt.day] = scEnt.ft1Attendance;
+          this.ft2Data[scEnt.day] = scEnt.ft2Attendance;
         });
+
         this.createNewDataSet();
-        this.isFT1Disabled = this.ft1Data[this.today] ? true : false;
-        this.isFT2Disabled = this.ft2Data[this.today] ? true : false;
-    });
+
+        if (isPastMonths) {
+          this.isFT1Disabled = true;
+          this.isFT2Disabled = true;
+        } else {
+          this.isFT1Disabled = this.ft1Data[this.today] ? true : false;
+          this.isFT2Disabled = this.ft2Data[this.today] ? true : false;
+        }
+      });
   }
 
   submit(featureTeam) {
-    console.log(featureTeam);
-
     const momentDate = moment(new Date());
     const yearAndMonth = momentDate.format('MMM YY');
+
+    if (featureTeam === 'FT1') {
+      this.ft2Value = null;
+    } else if (featureTeam === 'FT2') {
+      this.ft1Value = null;
+    }
 
     const scrumEntity: ScrumAttendance = {
       day: this.today,
@@ -71,22 +93,13 @@ export class ScrumComponent {
     };
 
     this.scrumService.saveScrumAttendance(scrumEntity).subscribe(response => {
+      response.forEach(scEnt => {
+        this.ft1Data[scEnt.day] = scEnt.ft1Attendance;
+        this.ft2Data[scEnt.day] = scEnt.ft2Attendance;
+      });
 
-        response.forEach(scEnt => {
-            this.ft1Data[scEnt.day] = scEnt.ft1Attendance;
-            this.ft2Data[scEnt.day] = scEnt.ft2Attendance;
-        });
-
-        this.initialLoad(this.getNoOfDays());
+      this.initialLoad(moment(moment()).daysInMonth());
     });
-
-    // if (featureTeam === 'FT1') {
-    //   this.ft1Data[this.today] = this.ft1Value;
-    //   this.initialLoad(this.getNoOfDays());
-    // } else if (featureTeam === 'FT2') {
-    //   this.ft2Data[this.today] = this.ft2Value;
-    //   this.initialLoad(this.getNoOfDays());
-    // }
   }
 
   createNewDataSet() {
@@ -94,13 +107,13 @@ export class ScrumComponent {
       labels: this.labelArray,
       datasets: [
         {
-          label: 'FT-1 - Gaurav-Sridhar-Prash-Suke-Bisw',
+          label: 'FT-1: Gaurav-Sridhar-Prash-Suke-Bisw',
           backgroundColor: '#42A5F5',
           borderColor: '#1E88E5',
           data: this.ft1Data
         },
         {
-          label: 'FT-2 - Dash-James-Rav-Vinay-Basa',
+          label: 'FT-2: Dash-James-Rav-Vinay-Basa',
           backgroundColor: '#9CCC65',
           borderColor: '#7CB342',
           data: this.ft2Data
@@ -109,24 +122,12 @@ export class ScrumComponent {
     };
   }
 
-  getNoOfDays(): Number {
-    console.log(moment().day() + 1);
-    const today = new Date();
-    const month = today.getMonth();
-    const daysInthisMonth = this.daysInMonth(month + 1, today.getFullYear());
-    return daysInthisMonth;
-  }
-
-  daysInMonth(month, year) {
-    return new Date(year, month, 0).getDate();
-  }
-
   thisDay() {
     this.today = moment().date();
     this.todayDate = moment().format('LL');
     this.loadMonthsDropdown();
     this.monthSelected = null;
-    this.initialLoad(this.getNoOfDays());
+    this.initialLoad(moment(moment()).daysInMonth());
   }
 
   loadMonthsDropdown() {
@@ -142,6 +143,7 @@ export class ScrumComponent {
   }
 
   monthSelection() {
-    console.log('this.monthSelected', this.monthSelected);
+    this.populateXAxisLabels( moment(this.monthSelected, 'MMMYY').daysInMonth());
+    this.loadValuesBasedOnMonthYear(this.monthSelected, true);
   }
 }
